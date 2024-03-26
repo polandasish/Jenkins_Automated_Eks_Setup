7066e27692fff3ee5fa43dbb307ffdcae7f5276c
polandasish <poland.asish@gmail.com>
2024-03-27 00:04:25 +0100

    commit for Jenkins setup

---
+++ Jenkins_setup/locals.tf
@@ -0,0 +1,20 @@
+locals {
+  inbound-rules = [
+    { port     = 80
+      protocol = "tcp"
+    },
+    {
+      port     = 443
+      protocol = "tcp"
+    },
+    {
+      port     = 8080
+      protocol = "tcp"
+    },
+    {
+      port     = 22
+      protocol = "tcp"
+    }
+
+  ]
+}
